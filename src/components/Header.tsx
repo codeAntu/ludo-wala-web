634cd2e5d30c530a3e8271277f7a5f4eac15c003
Abinash Karmakar <codeAbinash@gmail.com>
2024-08-13 21:12:30 +0530

    chore: Update header component with additional navigation options

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,31 @@
-import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 const options = [
   {
     name: 'Home',
     href: '/',
   },
+  {
+    name: 'Reft & Earn',
+    href: '/refer',
+  },
+  {
+    name: "FAQ's",
+    href: '/faqs',
+  },
+  {
+    name: 'Ludo Tournament',
+    href: '/ludo',
+  },
 ]
 
 export default function Header() {
   return (
     <div className='flex w-full items-center justify-center'>
-      <div className='w-full max-w-screen-xl bg-white/50'>
-        <img src='./icon.png' alt='icon' className='mx-auto size-16' />
-        <div>
+      <div className='flex w-full max-w-[1200px] items-center justify-between'>
+        <img src='./icon.png' alt='icon' className='size-14' />
+        <div className='flex gap-5'>
           {options.map((option) => (
             <Option key={option.name} name={option.name} href={option.href} />
           ))}
@@ -23,8 +36,13 @@ export default function Header() {
 }
 
 function Option({ name, href }: { name: string; href: string }) {
+  const location = useLocation()
+
   return (
-    <Link to={href} className='text-white/50 hover:text-white'>
+    <Link
+      to={href}
+      className={`text-[0.7rem] font-medium uppercase ${location.pathname === href ? 'text-white' : 'text-white/70'}`}
+    >
       {name}
     </Link>
   )
