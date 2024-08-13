0a2f2a71c5f2636bff81619ed31c753e3808eb9c
Ananta <codeantu@gmail.com>
2024-08-13 21:06:10 +0530

    feat: Update badges component and add images

--- src/components/Badges.tsx
+++ src/components/Badges.tsx
@@ -1,13 +1,13 @@
 export default function Badges() {
   return (
     <div className='w-full'>
-      <div>
-        <img src='./imgs/badges/image.png' alt='' />
-        <img src='./imgs/badges/image.1.png' alt='' />
-        <img src='./imgs/badges/image.2.png' alt='' />
-        <img src='./imgs/badges/image.3.png' alt='' />
-        <img src='./imgs/badges/image.4.png' alt='' />
-        <img src='./imgs/badges/image.5.png' alt='' />
+      <div className="flex flex-col gap-3 items-center justify-center w-full py-7 bg-red-500">
+        <img src='./images/badges/image.png' alt='' />
+        <img src='./images/badges/image.1.png' alt='' />
+        <img src='./images/badges/image.2.png' alt='' />
+        <img src='./images/badges/image.3.png' alt='' />
+        <img src='./images/badges/image.4.png' alt='' />
+        <img src='./images/badges/image.5.png' alt='' />
       </div>
     </div>
   )
