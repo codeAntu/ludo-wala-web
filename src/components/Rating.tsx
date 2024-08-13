8bcff583e367b3c126114aed8ddd587e5a2d1429
Abinash Karmakar <codeAbinash@gmail.com>
2024-08-13 23:29:05 +0530

    Add some components and rating component

---
+++ src/components/Rating.tsx
@@ -0,0 +1,14 @@
+export default function Rating() {
+  return (
+    <div className='flex items-center justify-center'>
+      <div className='mw flex w-full flex-col items-center justify-center'>
+        <p className='heading'>Our Players</p>
+        <p className='sub-heading'>Trust Ludo Wala</p>
+        <p className='body-content'>
+          Play real money games in India on highly trusted money game app Rush. Check out reviews from real users below.
+        </p>
+        <img src='./images/rating.png' alt='rating' className='mt-5 sm:mt-10' />
+      </div>
+    </div>
+  )
+}
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,26 +1,12 @@
+import Rating from '@/components/Rating'
 import Header from '../components/Header'
 
 export default function HomeScreen() {
   return (
     <div>
       <Header />
-      <div className='pt-20'>
-        <p className='text-5xl grad font-semibold text-white'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure alias nesciunt unde illum ab ipsa voluptatem.
-          Accusamus earum, id quam veniam corrupti reiciendis consequuntur quasi. Neque nihil facere et minus recusandae
-          nulla in! Dolorem minima quis quaerat dolore totam nesciunt! Iste accusantium, doloremque inventore illum
-          dolor autem minus nesciunt! Qui atque tempora laudantium repudiandae consequuntur magnam, excepturi a! Quo
-          repellat quidem minima qui sit saepe blanditiis fuga dolorum ea sapiente nam quasi reiciendis ipsum autem sunt
-          consectetur delectus deleniti molestiae assumenda consequatur aspernatur hic dicta, unde laudantium? Cum
-          perferendis quisquam harum odio saepe voluptatem esse excepturi, quae fugit omnis id veniam sit ipsa ratione
-          explicabo commodi eaque similique consequuntur placeat illum sequi velit. Facilis enim quasi quia atque
-          quibusdam. Officiis itaque impedit, in quia quasi amet hic, laudantium aspernatur delectus voluptatem repellat
-          nemo voluptates modi temporibus. Dicta veritatis fugit maxime. Obcaecati consectetur illum repudiandae
-          similique illo animi culpa labore omnis, praesentium voluptatem architecto quo in dolores at vitae pariatur
-          ex. Dolorum, neque aliquid dolor inventore eum quis velit natus iste in ut assumenda architecto, non repellat,
-          beatae numquam ab deleniti. Vitae nisi, dignissimos ab distinctio aut iusto rerum explicabo. Omnis eaque
-          officiis aut amet similique alias illo doloribus quidem dolor.
-        </p>
+      <div className='px-5 pt-20 sm:px-20'>
+        <Rating />
       </div>
     </div>
   )
